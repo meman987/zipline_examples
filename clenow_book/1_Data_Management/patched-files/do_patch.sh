f8b403baf05b8ff27dd28d2ea14a8008023224a4
me <p@gizur.com>
2024-10-11 14:50:03 +0200

    publish notes

---
+++ clenow_book/1_Data_Management/patched-files/do_patch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#   /home/me/.local/lib/python3.10/site-packages
+
+
+if [ $# -ne 1 ]; then
+    echo "usage: ./do_patch.sh <site-packages-path>"
+    exit 0
+fi
+
+cp assets.py         $1/zipline/assets/
+cp calendar_utils.py $1/zipline/utils/
+
+cp  exchange_calendar.py $1/exchange_calendars/
+
+cp extension.py ~/.zipline/
+
